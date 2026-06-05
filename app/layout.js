55924a8378927a8247bcb8fdf4e994e24d5e8f1b
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2026-06-06 00:10:43 +0330

    setup root layout with Navbar, Footer, and global styles

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import "../styles/globals.css";
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+
+
+export const metadata = {
+    title: "World Explorer",
+    description: "A Next.js country explorer project",
+  };
+  
+
+  export default function RootLayout ({ children }){
+    return(
+        <html lang='en' suppressHydrationWarning>
+            <body>
+                <Navbar/>
+                    <main className='container mx-auto px-4'> {children} </main>
+                <Footer/>
+            </body>
+
+        </html>
+    )
+  }
